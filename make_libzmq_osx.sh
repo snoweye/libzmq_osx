64cfcab0584d973a65d5c39728c9f85b324610d0
wccsnow <wccsnow@gmail.com>
2018-01-06 16:56:24 -0500

    add .travis.yml

--- make_libzmq_osx.sh
+++ make_libzmq_osx.sh
@@ -2,7 +2,16 @@
 
 cd libzmq
 ./autogen.sh
-./configure
-make
+./configure \
+  --enable-static=yes \
+  --enable-shared=no \
+  --without-docs \
+  --without-documentation \
+  --disable-pedantic \
+  --disable-eventfd \
+  --disable-perf \
+  --disable-curve-keygen \
+  --disable-curve
+make -j 4
 make check
 sudo make install
